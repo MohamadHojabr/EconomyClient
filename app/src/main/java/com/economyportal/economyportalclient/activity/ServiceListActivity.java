6af1bad27983f9034b09a2efffe14b153e4efa28
mohamadhojabr <s.mohamad.hojabr>
2016-11-02 21:24:25 +0330

    serviceList

---
+++ app/src/main/java/com/economyportal/economyportalclient/activity/ServiceListActivity.java
@@ -0,0 +1,96 @@
+package com.economyportal.economyportalclient.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import com.economyportal.economyportalclient.R;
+import com.economyportal.economyportalclient.Services.IServices.ITagService;
+import com.economyportal.economyportalclient.Services.ServiceConfig;
+import com.economyportal.economyportalclient.adapter.TagAdapter;
+import com.economyportal.economyportalclient.model.Tag;
+import com.economyportal.economyportalclient.utility.ClickListener;
+import com.economyportal.economyportalclient.utility.RecyclerTouchListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class ServiceListActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private TagAdapter mAdapter;
+    private ArrayList<Tag> data;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        ///
+
+
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), recyclerView, new ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Tag movie = data.get(position);
+                Toast.makeText(getApplicationContext(), movie.getName() + " is selected!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+                Tag movie = data.get(position);
+                Toast.makeText(getApplicationContext(), movie.getId() + " در سال!", Toast.LENGTH_SHORT).show();
+            }
+        }));
+
+        GotoTagService();
+    }
+
+    private void GotoTagService(){
+
+        ITagService tag_client = ServiceConfig.createService(ITagService.class);
+        final Call<List<Tag>> tag_call = tag_client.tags();
+
+        tag_call.enqueue(new Callback<List<Tag>>() {
+            @Override
+            public void onResponse(Call<List<Tag>> call, Response<List<Tag>> response) {
+
+                data = (ArrayList<Tag>) response.body();
+                mAdapter = new TagAdapter(data);
+                recyclerView.setAdapter(mAdapter);
+            }
+            @Override
+            public void onFailure(Call<List<Tag>> call, Throwable t) {
+                Log.d("NOOOOOOO" , t.getMessage());
+            }
+        });
+    }
+}
--- app/src/main/java/com/economyportal/economyportalclient/activity/ServicesActivity.java
+++ app/src/main/java/com/economyportal/economyportalclient/activity/ServicesActivity.java
@@ -4,30 +4,15 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
+
 import com.economyportal.economyportalclient.R;
-import com.economyportal.economyportalclient.adapter.TagAdapter;
-import com.economyportal.economyportalclient.model.Tag;
-import com.economyportal.economyportalclient.Services.IServices.ITagService;
-import com.economyportal.economyportalclient.Services.ServiceConfig;
-import com.economyportal.economyportalclient.utility.ClickListener;
-import com.economyportal.economyportalclient.utility.RecyclerTouchListener;
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
-import java.util.ArrayList;
-import java.util.List;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 /**
  * Created by mohamad on 10/29/2016.
@@ -35,15 +20,8 @@ import retrofit2.Response;
 
 public class ServicesActivity extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
-    private TagAdapter mAdapter;
-    private ArrayList<Tag> data;
-
-
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
+    TextView title;
+    Button request_brokerage;
     private GoogleApiClient client;
 
 
@@ -54,53 +32,22 @@ public class ServicesActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String searchString = intent.getExtras().getString("search_string");
-        final TextView txtView = (TextView) findViewById(R.id.textView2);
-        txtView.setText(searchString);
-
-        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
-        recyclerView.setLayoutManager(mLayoutManager);
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), recyclerView, new ClickListener() {
-            @Override
-            public void onClick(View view, int position) {
-                Tag movie = data.get(position);
-                Toast.makeText(getApplicationContext(), movie.getName() + " is selected!", Toast.LENGTH_SHORT).show();
+        title = (TextView) findViewById(R.id.services_title);
+        title.setText(searchString);
+        request_brokerage = (Button) findViewById(R.id.request_brokerage);
+        request_brokerage.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(ServicesActivity.this, ServiceListActivity.class);
+                startActivity(i);
             }
-
-            @Override
-            public void onLongClick(View view, int position) {
-                Tag movie = data.get(position);
-                Toast.makeText(getApplicationContext(), movie.getId() + " در سال!", Toast.LENGTH_SHORT).show();
-            }
-        }));
-
-        GotoTagService();
+        });
 
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    private void GotoTagService(){
 
-        ITagService tag_client = ServiceConfig.createService(ITagService.class);
-        final Call<List<Tag>> tag_call = tag_client.tags();
-
-        tag_call.enqueue(new Callback<List<Tag>>() {
-            @Override
-            public void onResponse(Call<List<Tag>> call, Response<List<Tag>> response) {
-
-                data = (ArrayList<Tag>) response.body();
-                mAdapter = new TagAdapter(data);
-                recyclerView.setAdapter(mAdapter);
-            }
-            @Override
-            public void onFailure(Call<List<Tag>> call, Throwable t) {
-                Log.d("NOOOOOOO" , t.getMessage());
-            }
-        });
-    }
 
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
